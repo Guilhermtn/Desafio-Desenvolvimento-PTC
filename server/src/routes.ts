8a8336af97ce29eb827df3dc0246c4d3fa1ec31f
guilhermetn <gui222013@hotmail.com>
2026-05-01 01:05:56 -0300

    feat: add calcados routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,15 @@
 import express from "express";
-import {  readAllUsers } from "./controllers/UserController";
-
+import { readAllUsers } from "./controllers/UserController";
+import { createCalcado, readAllCalcados, updateCalcado, deleteCalcado } from "./controllers/CalcadosController";
 
 const routes = express.Router();
 
 routes.get("/users", readAllUsers);
 
+// Rotas de calçados
+routes.post("/calcados", createCalcado);       // POST 
+routes.get("/calcados", readAllCalcados);       // GET 
+routes.patch("/calcados/:id", updateCalcado);   // PATCH 
+routes.delete("/calcados/:id", deleteCalcado);  // DELETE 
 
 export default routes;
