569755f1d71b8f49a29d1066856ed1e77e7f7bbf
guilhermetn <gui222013@hotmail.com>
2026-05-01 01:34:05 -0300

    feat: add rotas de calcados (tamanho, marca, total)

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,26 @@
 import express from "express";
 import { readAllUsers } from "./controllers/UserController";
-import { createCalcado, readAllCalcados, updateCalcado, deleteCalcado } from "./controllers/CalcadosController";
+import { 
+  createCalcado, 
+  readAllCalcados, 
+  updateCalcado, 
+  deleteCalcado,
+  getByTamanho,
+  getByMarca,
+  getTotalPares 
+} from "./controllers/CalcadosController";
 
 const routes = express.Router();
 
 routes.get("/users", readAllUsers);
 
 // Rotas de calçados
-routes.post("/calcados", createCalcado);       // POST 
-routes.get("/calcados", readAllCalcados);       // GET 
-routes.patch("/calcados/:id", updateCalcado);   // PATCH 
-routes.delete("/calcados/:id", deleteCalcado);  // DELETE 
+routes.post("/calcados", createCalcado);         
+routes.patch("/calcados/:id", updateCalcado);   
+routes.delete("/calcados/:id", deleteCalcado);  
+routes.get("/calcados/tamanho/:tamanho", getByTamanho);  
+routes.get("/calcados/marca/:marca", getByMarca);         
+routes.get("/calcados/total", getTotalPares);              
+routes.get("/calcados", readAllCalcados);  
 
 export default routes;
